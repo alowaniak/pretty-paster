43e7f364210e909bd32c7733229bb0b0f3dbc700
alowaniak <alowanak@hotmail.com>
2019-04-08 13:53:36 +0200

    Add chainability for Prettifiers

--- src/prettypaster/Prettifier.java
+++ src/prettypaster/Prettifier.java
@@ -1,6 +1,7 @@
 package prettypaster;
 
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * A {@link Prettifier} is able to "{@link #prettify(String) prettify}" content.
@@ -8,8 +9,19 @@ import java.util.Optional;
 @FunctionalInterface
 public interface Prettifier {
 
+    static Prettifier chainable(Prettifier... chain) {
+        if(chain.length == 0) throw new IllegalArgumentException();
+        return in -> {
+            var curIn = new AtomicReference<String>();
+            for (Prettifier p : chain) {
+                p.prettify(curIn.get() == null ? in : curIn.get()).ifPresent(curIn::set);
+            }
+            return Optional.ofNullable(curIn.get());
+        };
+    }
+
     /**
-     * "Prettifies" given input, meaning it will turn it into a nicer/more readable format.
+     * "Prettifies" given input, meaning it will turn it into a nicer/more readable format or decode it.
      *
      * @param in the content to prettify
      * @return an {@link Optional} containing the prettified {@code content}.
---
+++ test/prettypaster/PrettifierTest.java
@@ -0,0 +1,88 @@
+package prettypaster;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrettifierTest {
+
+    @Test void emptyChainIsIllegal() {
+        try {
+            var ignored = Prettifier.chainable();
+            fail("Prettifier#chainable without any arguments should fail.");
+        } catch (IllegalArgumentException e) {
+            // success
+        }
+    }
+
+    @Test void chainableWithSinglePrettifierDoesntFunctionallyChangeFromSinglePrettifier() {
+        var expectedOut = Optional.of(UUID.randomUUID().toString());
+
+        var mock = new Prettifier() {
+            String actualIn;
+            @Override public Optional<String> prettify(String in) {
+                actualIn = in;
+                return expectedOut;
+            }
+        };
+
+        var expectedIn = UUID.randomUUID().toString();
+        assertEquals(expectedOut, Prettifier.chainable(mock).prettify(expectedIn));
+        assertEquals(expectedIn, mock.actualIn);
+    }
+
+    @Test void ifFirstPrettifierReturnsAnEmptyThenSecondPrettifierGetsOriginalInputAndItsResultIsChainResult() {
+        String originalInput = UUID.randomUUID().toString();
+        var expectedOut = Optional.of(UUID.randomUUID().toString());
+
+        Prettifier first = s -> Optional.empty();
+        var second = new Prettifier() {
+            boolean calledWithOriginalInput;
+            @Override public Optional<String> prettify(String in) {
+                if (in.equals(originalInput)) calledWithOriginalInput = true;
+                return expectedOut;
+            }
+        };
+
+        var actualOut = Prettifier.chainable(first, second).prettify(originalInput);
+        assertEquals(expectedOut, actualOut);
+        assertTrue(second.calledWithOriginalInput);
+    }
+
+    @Test void ifSecondPrettifierReturnsAnEmptyThenFirstPrettifiersResultIsChainResult() {
+        var originalInput = UUID.randomUUID().toString();
+        var expectedOut = Optional.of(UUID.randomUUID().toString());
+
+        var first = new Prettifier() {
+            boolean calledWithOriginalInput;
+
+            @Override public Optional<String> prettify(String in) {
+                if (in.equals(originalInput)) calledWithOriginalInput = true;
+                return expectedOut;
+            }
+        };
+        Prettifier second = s -> Optional.empty();
+
+        var actualOut = Prettifier.chainable(first, second).prettify(originalInput);
+        assertEquals(expectedOut, actualOut);
+        assertTrue(first.calledWithOriginalInput);
+    }
+
+    @Test void resultOfFirstPretifierIsChainedToSecondPrettifier() {
+        var firstsResult = UUID.randomUUID().toString();
+        Prettifier first = s -> Optional.of(firstsResult);
+        var second = new Prettifier() {
+            boolean calledWithFirstsResult;
+            @Override public Optional<String> prettify(String in) {
+                if (in.equals(firstsResult)) calledWithFirstsResult = true;
+                return Optional.empty();
+            }
+        };
+
+        Prettifier.chainable(first, second).prettify("");
+        assertTrue(second.calledWithFirstsResult);
+    }
+}
